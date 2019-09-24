2c0116e6792b1ece3da2c060d5eb76934c2efb5f
houstonwalley <55718068+houstonwalley@users.noreply.github.com>
2019-09-24 11:16:49 -0500

    Create Autori.java

---
+++ Java/Autori.java
@@ -0,0 +1,17 @@
+public class Autori {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		String line = in.nextLine();
+		String[] x = line.split("-");
+		String result = "";
+		for (int i = 0; i < x.length; i++) {
+			result += x[i].substring(0,1);
+		}
+		System.out.print(result);
+		in.close();
+		System.exit(0);;
+	}
+
+}
