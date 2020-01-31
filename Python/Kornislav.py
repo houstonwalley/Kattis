8bcbfc3bb44e32277504848e40a86ee874c05994
houstonwalley <walleyhouston@gmail.com>
2020-01-30 22:37:14 -0600

    Adds two python programs

---
+++ Python/Kornislav.py
@@ -0,0 +1,5 @@
+n = input().split()
+for i in range(len(n)):
+	n[i] = int(n[i])
+n.sort()
+print(n[0] * n[2])
---
+++ Python/NoDup.py
@@ -0,0 +1,5 @@
+n = input().split()
+if len(n) == len(set(n)):
+	print("yes")
+else:
+	print("no")
